b796003bea25ab8a58ecb8666aef06aa79e05ff3
Jeff B <jeff.r.barker@gmail.com>
2018-07-03 15:32:46 -0700

    c sharp page 28 exercise
    Completed console program for page 28 of the C# course.

---
+++ C#/page28exercise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace DailyReportExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy \r\n-----------------\r\nStudent Daily Report\r\n____________________\r\n");
+            Console.WriteLine("What course are you on?");
+            string courseName = Console.ReadLine();
+            Console.WriteLine("\r\nWhat page number?");
+            string pageNumber = Console.ReadLine();
+            byte pageNo = Convert.ToByte(pageNumber);
+            Console.WriteLine("\r\nDo you need help with anything? Please answer \"true\" or \"false\".");
+            string helpIndicator = Console.ReadLine();
+            bool helpInd = Convert.ToBoolean(helpIndicator);
+            Console.WriteLine("\r\nWere there any positive experiences you'd like to share? Please give specifics.");
+            string posExp = Console.ReadLine();
+            Console.WriteLine("\r\nIs there any other feedback you'd like to provide? Please be specific.");
+            string feedBack = Console.ReadLine();
+            Console.WriteLine("\r\nHow many hours did you study today?");
+            string hourNumber = Console.ReadLine();
+            byte hourNo = Convert.ToByte(hourNumber);
+
+            Console.WriteLine("\r\n__________________________\r\nThank you for your answers. An instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+
+
+        }
+    }
+}
