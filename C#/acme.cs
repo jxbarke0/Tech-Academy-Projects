767c30d0f1cf35e64191103d2705ae273b1764ab
Jeff B <jeff.r.barker@gmail.com>
2018-07-03 12:39:53 -0700

    drill from p.23
    Drill from c# course, page 23.

---
+++ C#/acme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Drillp23
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \r\nRemember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
