702104284d94810bb2a2723d1e6cea611e8df694
Jeff B <jeff.r.barker@gmail.com>
2018-08-24 16:44:27 -0700

    MVC User Role
    Created simple web application using common user security authentication.

---
+++ C#/MVCUserRole/MVCUserRole/Controllers/RoleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUserRole.Controllers
+{
+    public class RoleController : Controller
+    {
+        // GET: Role
+        public ActionResult Index()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+
+
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+
+        }
+    }
+}
